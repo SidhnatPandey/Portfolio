7d9cce3191cbddafb1c93994a2d1b6792fee29e9
sidhant pandey <sidhantpandeynp6@gmail.com>
2025-01-15 21:39:53 +0530

    Portfolio created

---
+++ src/components/Projects.jsx
@@ -0,0 +1,111 @@
+import { Image, Text } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { animate, useMotionValue } from "framer-motion";
+import { motion } from "framer-motion-3d";
+import { atom, useAtom } from "jotai";
+import { useEffect, useRef } from "react";
+
+export const projects = [
+  {
+    title: "Secreto",
+    url: "https://secreto-delta.vercel.app/",
+    image: "projects/Design.png",
+    description: "For Secretly Real time communication",
+  },
+  {
+    title: "QR Code Generator",
+    url: "https://github.com/SidhnatPandey/aws-qr-code",
+    image: "projects/oj.jpg",
+    description: "Generate qr code of an url",
+  },
+  {
+    title: "TSS Tracker Website",
+    url: "https://github.com/SidhnatPandey/SIH-2022-Web-App-The-Special-School-",
+    image: "projects/HE.png",
+    description: "Dummy Project 3 Details",
+  },
+  {
+    title: "Jobbey API",
+    url: "https://jobbey-api-frontend.vercel.app/sign-in?redirectUrl=/",
+    image: "projects/reactjs.jpg",
+    description: "College Mini Project.",
+  }
+];
+const Project = (props) => {
+  const { project, highlighted } = props;
+
+  const background = useRef();
+  const bgOpacity = useMotionValue(0.4);
+
+  useEffect(() => {
+    animate(bgOpacity, highlighted ? 0.7 : 0.4);
+  }, [highlighted]);
+
+  useFrame(() => {
+    background.current.material.opacity = bgOpacity.get();
+  });
+
+  return (
+    <group {...props}>
+      <mesh
+        position-z={-0.001}
+        // onClick={() => window.open(project.url, "_blank")}
+        ref={background}
+      >
+        <planeGeometry args={[2.2, 2]} />
+        <meshBasicMaterial color="black" transparent opacity={0.4} />
+      </mesh>
+      <Image
+        scale={[2, 1.2, 1]}
+        url={project.image}
+        toneMapped={false}
+        position-y={0.3}
+      />
+      <Text
+        maxWidth={2}
+        anchorX={"left"}
+        anchorY={"top"}
+        fontSize={0.2}
+        position={[-1, -0.4, 0]}
+      >
+        {project.title.toUpperCase()}
+      </Text>
+      <Text
+        maxWidth={2}
+        anchorX="left"
+        anchorY="top"
+        fontSize={0.1}
+        position={[-1, -0.6, 0]}
+      >
+        {project.description}
+      </Text>
+    </group>
+  );
+};
+
+export const currentProjectAtom = atom(0);
+
+export const Projects = () => {
+  const { viewport } = useThree();
+  const [currentProject] = useAtom(currentProjectAtom);
+
+  return (
+    <group position-y={-viewport.height * 2 + 1}>
+      {projects.map((project, index) => (
+        <motion.group
+          key={"project_" + index}
+          position={[index * 2.5, 0, -3]}
+          animate={{
+            x: 0 + (index - currentProject) * 2.5,
+            y: currentProject === index ? 0 : -0.1,
+            z: currentProject === index ? -2 : -3,
+            rotateX: currentProject === index ? 0 : -Math.PI / 3,
+            rotateZ: currentProject === index ? 0 : -0.1 * Math.PI,
+          }}
+        >
+          <Project project={project} highlighted={index === currentProject} />
+        </motion.group>
+      ))}
+    </group>
+  );
+};
